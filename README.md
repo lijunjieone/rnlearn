8e469f01b32720e979c255441ab03a7a489417d8
lijunjie <lijunjieone@gmail.com>
2018-10-12 17:28:54 +0800

    增加webview的另外一个例子

---
+++ README.md
@@ -0,0 +1,4 @@
+# 目标
+一些react native的例子
+
+
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import React,{Component } from 'react';
 
 import {Text,Image,View,StyleSheet,FlatList,WebView,
     Button,
+    TouchableOpacity,
+    TextInput,
 
 } from 'react-native';
 
@@ -20,7 +22,8 @@ export default class App extends Component {
                         {/* <Blink text="this is a test"></Blink> */}
                         {/* <LinearLayoutTest> </LinearLayoutTest> */}
                         {/* <FlatListTest></FlatListTest> */}
-                        <ScaledWebView></ScaledWebView>
+                        {/* <ScaledWebView></ScaledWebView> */}
+                        <WebViewExample></WebViewExample>
 
 
             </View>
@@ -138,6 +141,152 @@ class FlatListTest extends Component {
 }
 
 
+var TEXT_INPUT_REF = 'urlInput';
+var WEBVIEW_REF = 'webview';
+var DEFAULT_URL = 'https://m.baidu.com';
+
+
+class WebViewExample extends React.Component {
+    state = {
+      url: DEFAULT_URL,
+      status: 'No Page Loaded',
+      backButtonEnabled: false,
+      forwardButtonEnabled: false,
+      loading: true,
+      scalesPageToFit: true,
+    };
+  
+    inputText = '';
+  
+    handleTextInputChange = (event) => {
+      var url = event.nativeEvent.text;
+      if (!/^[a-zA-Z-_]+:/.test(url)) {
+        url = 'http://' + url;
+      }
+      this.inputText = url;
+    };
+  
+    render() {
+      this.inputText = this.state.url;
+  
+      return (
+        <View style={[styles.container]}>
+          <View style={[styles.addressBarRow]}>
+            <TouchableOpacity
+              onPress={this.goBack}
+              style={this.state.backButtonEnabled ? styles.navButton : styles.disabledButton}>
+              <Text>
+                 {'<'}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={this.goForward}
+              style={this.state.forwardButtonEnabled ? styles.navButton : styles.disabledButton}>
+              <Text>
+                {'>'}
+              </Text>
+            </TouchableOpacity>
+            <TextInput
+              ref={TEXT_INPUT_REF}
+              autoCapitalize="none"
+              defaultValue={this.state.url}
+              onSubmitEditing={this.onSubmitEditing}
+              onChange={this.handleTextInputChange}
+              clearButtonMode="while-editing"
+              style={styles.addressBarTextInput}
+            />
+            <TouchableOpacity onPress={this.pressGoButton}>
+              <View style={styles.goButton}>
+                <Text>
+                   Go!
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <WebView
+            ref={WEBVIEW_REF}
+            automaticallyAdjustContentInsets={false}
+            style={styles.webView}
+            source={{uri: this.state.url}}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            decelerationRate="normal"
+            onNavigationStateChange={this.onNavigationStateChange}
+            onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
+            startInLoadingState={true}
+            scalesPageToFit={this.state.scalesPageToFit}
+          />
+          <View style={styles.statusBar}>
+            <Text style={styles.statusBarText}>{this.state.status}</Text>
+          </View>
+        </View>
+      );
+    }
+  
+    goBack = () => {
+      this.refs[WEBVIEW_REF].goBack();
+    };
+  
+    goForward = () => {
+      this.refs[WEBVIEW_REF].goForward();
+    };
+  
+    reload = () => {
+      this.refs[WEBVIEW_REF].reload();
+    };
+  
+    onShouldStartLoadWithRequest = (event) => {
+      // Implement any custom loading logic here, don't forget to return!
+      return true;
+    };
+  
+    onNavigationStateChange = (navState) => {
+      this.setState({
+        backButtonEnabled: navState.canGoBack,
+        forwardButtonEnabled: navState.canGoForward,
+        url: navState.url,
+        status: navState.title,
+        loading: navState.loading,
+        scalesPageToFit: true
+      });
+    };
+  
+    onSubmitEditing = (event) => {
+      this.pressGoButton();
+    };
+  
+    pressGoButton = () => {
+      var url = this.inputText.toLowerCase();
+      if (url === this.state.url) {
+        this.reload();
+      } else {
+        this.setState({
+          url: url,
+        });
+      }
+      // dismiss keyboard
+      this.refs[TEXT_INPUT_REF].blur();
+    };
+  }
+  
+//   class Button extends React.Component {
+//     _handlePress = () => {
+//       if (this.props.enabled !== false && this.props.onPress) {
+//         this.props.onPress();
+//       }
+//     };
+  
+//     render() {
+//       return (
+//         <TouchableWithoutFeedback onPress={this._handlePress}>
+//           <View style={styles.button}>
+//             <Text>{this.props.text}</Text>
+//           </View>
+//         </TouchableWithoutFeedback>
+//       );
+//     }
+//   }
+  
 
 class ScaledWebView extends React.Component {
     state = {
@@ -191,4 +340,11 @@ const styles = StyleSheet.create({
         color:'blue',
         fontSize:30,
     },
+
+
+    container: {
+        height: 800,
+        width:300,
+      },
+      
 });
