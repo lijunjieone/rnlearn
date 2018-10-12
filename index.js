63b08fdb70234b0adffb041ba7905339797b885a
lijunjie <lijunjieone@gmail.com>
2018-10-12 16:18:53 +0800

    hello world

---
+++ index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './src/main';
+
+// import WebViewExample from './src/webview';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
